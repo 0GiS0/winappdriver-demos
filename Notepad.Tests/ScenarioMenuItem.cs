4a93669921e8c22ab9cd603b7c6adb7ec236bcca
Gisela Torres <giselatb@outlook.com>
2019-03-04 16:33:01 +0000

    first commit

---
+++ Notepad.Tests/ScenarioMenuItem.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Notepad.Tests
+{
+    [TestClass]
+    public class ScenarioMenuItem : NotepadSession
+    {
+        [TestMethod]
+        public void MenuItemEdit()
+        {
+            // Select Edit -> Time/Date to get Time/Date from Notepad
+            Assert.AreEqual(string.Empty, editBox.Text);
+            session.FindElementByName("Edit").Click();
+            session.FindElementByXPath($"//MenuItem[starts-with(@Name, \"Time/Date\")]").Click();
+            string timeDateString = editBox.Text;
+            Assert.AreNotEqual(string.Empty, timeDateString);
+
+            // Select all text, copy, and paste it twice using MenuItem Edit -> Select All, Copy, and Paste
+            session.FindElementByName("Edit").Click();
+            session.FindElementByXPath($"//MenuItem[starts-with(@Name, \"Select All\")]").Click();
+            session.FindElementByName("Edit").Click();
+            session.FindElementByXPath($"//MenuItem[starts-with(@Name, \"Copy\")]").Click();
+            session.FindElementByName("Edit").Click();
+            session.FindElementByXPath($"//MenuItem[starts-with(@Name, \"Paste\")]").Click();
+            session.FindElementByName("Edit").Click();
+            session.FindElementByXPath($"//MenuItem[starts-with(@Name, \"Paste\")]").Click();
+
+            // Verify that the Time/Date string is duplicated
+            Assert.AreEqual(timeDateString + timeDateString, editBox.Text);
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+    }
+}
